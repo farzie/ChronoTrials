a639abbd1a147876d90739f3076cdcf90acfa2f6
farzie <farizexemail@gmail.com>
2025-10-22 00:25:18 +0700

    bismilah

---
+++ scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_button_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_button_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/stage_1.tscn")
+
+
+func _on_button_setting_pressed() -> void:
+	get_node("SettingMenu").show()
+
+
+func _on_button_back_pressed() -> void:
+	get_node("SettingMenu").hide()
