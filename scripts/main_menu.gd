e1a490e9e910418ac5fad0baecd8fdf47c723104
farzie <farizexemail@gmail.com>
2025-10-31 21:06:51 +0700

    tes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,7 +6,7 @@ func _on_button_exit_pressed() -> void:
 
 
 func _on_button_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/stage_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/stage_1_eksplorasi.tscn")
 
 
 func _on_button_setting_pressed() -> void:
